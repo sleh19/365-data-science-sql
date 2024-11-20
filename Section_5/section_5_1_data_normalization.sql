94fb1c88459f5d92b2604d99e2a94b55de95cf9b
sleh19 <57458166+sleh19@users.noreply.github.com>
2024-11-20 21:39:16 +0100

    Update section_5_1_data_normalization.sql

--- Section_5/section_5_1_data_normalization.sql
+++ Section_5/section_5_1_data_normalization.sql
@@ -1,6 +1,6 @@
 -- First Normal Form
 
-create table class (
+create table class_unformalized (
 student_id serial,
 advisor varchar,
 room varchar,
@@ -9,7 +9,7 @@ class2 varchar,
 class3 varchar
 )
 
-insert into class (
+insert into class_unformalized (
 advisor,
 room,
 class1,
@@ -19,6 +19,20 @@ class3
 values
 ('Jones', 123, 'Biology', 'Chemistry', 'Physics')
 ('Smith', 131, 'English', 'Math', 'Library Science')
+
+- Unnormalized Form
+select * from class_unformalized;
+
+-- First Normalized Form (1NF)
+select c.student_id, c.advisor, c. room, t.*
+from class_unnormalized c
+cross join lateral (
+values
+(c.class1, 'class1'),
+(c.class2, 'class2'),
+(c.class3, 'class3')
+) ast (subject, class_num)
+order by student_id;
 
 -- Second Normal Form
 CREATE TABLE Orders (
