fb1be9f624b6075e04771b457f5a405aeb6d3f17
sleh19 <57458166+sleh19@users.noreply.github.com>
2024-11-20 12:06:07 +0100

    Update section_3_3_range.sql

--- Section_3/section_3_3_range.sql
+++ Section_3/section_3_3_range.sql
@@ -4,7 +4,16 @@ CREATE TABLE job_board (
   salary NUMERIC,
   salary_range numrange
 );
+/*
+Key Differences
 
+Feature	|| int4range	||numrange
+Data Type ||	Integer (int4)	|| Numeric (numeric)
+Precision	|| Whole numbers only	|| Decimal and fractional values allowed
+Use Case	|| Discrete ranges	|| Continuous ranges
+Storage Efficiency	|| More efficient for integer ranges	|| May use more storage due to precision
+
+*/
 INSERT INTO job_board (job, salary, salary_range)
 VALUES
   ('Engineer I', 120000, NUMRANGE(95000, 130000)),
